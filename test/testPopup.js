f89e61af317d9c11985800bbbd70c91d5be13782
Pashasc <pasha@safaribooksonline.com>
2016-03-19 15:52:32 -0700

    ignore test build files

--- test/testPopup.js
+++ test/testPopup.js
@@ -1,6 +1,5 @@
 import { keys, radix, FILTER_HIDE_CLASS, SELECTED_CLASS, SUSPEND_AFTER_MINS_DEFAULT } from '../src/js/constants.js';
 
-
 describe('true', function () {
     it('should be true', function () {
       console.log(sinon);
