1fd817cebf0d8b0a601aff9f90f90bf13d088b22
Pashasc <pasha@safaribooksonline.com>
2016-03-25 17:43:25 -0700

    ui improvements

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -13,17 +13,18 @@ function template (data) {
 		faviconUrl = data.faviconRenderUrl ? '<span class="favicon">' +
 		  '<img class="favicon" src="' + data.faviconRenderUrl + '" />' +
 		  '</span>' : '',
-		timeAgo = (data.time_ago === 0 || data.time_ago === undefined) ? '' : ((data.time_ago === 1) ? (data.time_ago + ' min') : (data.time_ago + ' mins'));
+		timeAgo = (data.time_ago === 0 || data.time_ago === undefined) ? '' : ((data.time_ago === 1) ? (data.time_ago + ' min') : (data.time_ago + ' mins')),
+		timeAgoSpan = (timeAgo === '') ? '' : '<span class="time-ago">' + timeAgo + '</span>';
 
 	return '<li id="' + data.id + '" class="' + suspendedClass + ' tab-item">' +
 		faviconUrl +
 		'<span class="title">' +
 		'<a href="#" class="js-title">' + data.title + '</a>' +
 		'</span>' +
-		'<span class="time-ago">' + timeAgo + '</span>' +
 		'<ul class="link-options">' +
+			'<li>' + timeAgoSpan + '</li>' +
 			'<li><a href="#" class="js-close-tab close-tab">' +
-				'<img src="' + chrome.extension.getURL('assets/bin.png') + '" title="Close tab" />' +
+				'<img src="' + chrome.extension.getURL('assets/bin.png') + '" title="Remove tab" />' +
 			'</a></li>' +
 			'<li>' + pinnedLink + '</li>' +
 			'<li>' + suspendedLink + '</li>' +
